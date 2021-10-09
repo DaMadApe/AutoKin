6f3480e4e27a2298c3dc0a307c68da127d6a05e6
damadape <danielsapien@gmail.com>
2021-10-09 18:10:46 -0500

    Experiment ready

--- experim0.py
+++ experim0.py
@@ -7,12 +7,15 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 # args
-n_samples = 50
-batch_size = 5
-epochs = 10
-learn_rate = 3e4
+n_samples = 10
+batch_size = 10
+epochs = 10000
+learn_rate = 3e-4
 n_mid_layers = 3
-mid_layer_size = 256
+mid_layer_size = 10
+bias = True
+crack_norm = True
+view_plot = True
 
 class Regressor(nn.Module):
 
@@ -21,31 +24,29 @@ class Regressor(nn.Module):
         super().__init__()
         #layers = OrderedDict()
         self.model = nn.Sequential(
-            #nn.Flatten(start_dim=1),
-            nn.Linear(1, mid_layer_size),
-            nn.ReLU(),
-            nn.Linear(mid_layer_size, mid_layer_size),
-            nn.ReLU(),
-            nn.Linear(mid_layer_size, 1),
-            nn.Softmax()
+            nn.Linear(input_dim, mid_layer_size, bias=bias),
+            nn.Tanh(),
+            nn.Linear(mid_layer_size, mid_layer_size, bias=bias),
+            nn.Tanh(),
+            nn.Linear(mid_layer_size, output_dim, bias=bias)
         )
 
     def forward(self, x):
-        out = (x+10)/20
-        #out = out.view(-)
+        if crack_norm:
+            out = (x+10)/20
         out = self.model(out)
         return out
 
 
 model = Regressor(1, 1, 3, 10)
 
-x = torch.linspace(-10, 10, n_samples)
+x = torch.linspace(-1, 1, n_samples).view(-1, 1)
 
 def f(x): return 3*x**3
 y = f(x)
 
 train_set = TensorDataset(x, y)#(x.view(-1,1), y.view(-1,1))
-train_loader = DataLoader(train_set, batch_size=batch_size)
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learn_rate)
@@ -58,12 +59,19 @@ for _ in progress:
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        progress.set_postfix(Loss=loss.item())
+    progress.set_postfix(Loss=loss.item())
 
-with torch.no_grad():
-    pred = model(x)
-fig, ax = plt.subplots()
-ax.scatter(x, y)
-ax.plot(x, pred)
+if view_plot:
+    x_plot = torch.linspace(-1, 1, n_samples).view(-1,1)
 
-plt.show()
+    with torch.no_grad():
+        pred = model(x_plot)
+    fig, ax = plt.subplots()
+    ax.plot(x_plot, f(x))
+    ax.plot(x_plot, pred)
+    ax.scatter(x, y)
+    ax.legend(['Target F',
+               'Model',
+               'Trainset'])
+
+    plt.show()
