fdbad47a8abece4efcb1f6e2705aebe27ed9b065
damadape <danielsapien@gmail.com>
2021-11-03 15:00:06 -0600

    Reorganizado como módulo

--- experim0.py
+++ experim0.py
@@ -1,37 +1,14 @@
-from typing import OrderedDict
 import torch
 from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import TensorDataset, DataLoader
-import logging
 import matplotlib.pyplot as plt
-
-"""
-TODO
-- Hacer variable número de capas
-- Meter tensorboard con guild
-"""
-# args
-lr = 1e-3
-depth = 3
-mid_layer_size = 10
-n_samples = 10
-batch_size = 10
-epochs = 1000
-
-input_dim = 1
-output_dim = 1
-x_min = 0
-x_max = 1
-view_plot = True
-
-def f(x): return 3*x**3 - 5*x**2 + 2*x
 
 
 class Regressor(nn.Module):
 
     def __init__(self, input_dim=1, output_dim=1,
-                 depth=1, mid_layer_size=10, activation=F.relu):
+                 depth=1, mid_layer_size=10, activation=torch.tanh):
         super().__init__()
 
         # Armar modelo
@@ -48,51 +25,74 @@ class Regressor(nn.Module):
         self.activation = activation
 
     def forward(self, x):
-        for layer in self.layers:
+        for layer in self.layers[:-1]:
             x = self.activation(layer(x))
+        x = self.layers[-1](x)
         return x
 
-"""
-Conjunto de datos
-"""
-x = torch.linspace(x_min, x_max, n_samples).view(-1, 1)
-y = f(x)
 
-train_set = TensorDataset(x, y)
-train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+if __name__ == '__main__':
+
+    torch.manual_seed(42)
+
+    # args
+    lr = 5e-3
+    depth = 10
+    mid_layer_size = 10
+    n_samples = 32
+    batch_size = 8
+    epochs = 2000
+
+    input_dim = 1
+    output_dim = 1
+    x_min = -1
+    x_max = 1
+    view_plot = True
+
+    #def f(x): return 3*x**3 - 3*x**2 + 2*x
+    def f(x): return torch.sin(6*x**2 - 6*x)
+
+    """
+    Conjunto de datos
+    """
+    x = torch.linspace(x_min, x_max, n_samples).view(-1, 1)
+    y = f(x)
+
+    train_set = TensorDataset(x, y)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 
 
-"""
-Entrenamiento
-"""
-model = Regressor(input_dim, 1, 3, 10)
+    """
+    Entrenamiento
+    """
+    model = Regressor(input_dim, 1, 3, 10)
 
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-for t in range(epochs):
-    for X, Y in train_loader:
-        pred = model(X)
-        loss = criterion(pred, Y)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    if t%(epochs//10) == 0:
-        print(f'Epoch {t}: Loss={loss.item()}')
+    for t in range(epochs):
+        for X, Y in train_loader:
+            pred = model(X)
+            loss = criterion(pred, Y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        if t%(epochs//10) == 0:
+            print(f'Epoch {t}: Loss={loss.item()}')
 
-"""
-Visualización de datos en una dimensión
-"""
-if view_plot:
-    x_plot = torch.linspace(x_min, x_max, n_samples).view(-1,1)
+    """
+    Visualización de datos en una dimensión
+    """
+    if view_plot:
+        x_plot = torch.linspace(x_min, x_max, 1000).view(-1,1)
 
-    with torch.no_grad():
-        pred = model(x_plot)
-    fig, ax = plt.subplots()
-    ax.plot(x_plot, f(x))
-    ax.plot(x_plot, pred)
-    ax.scatter(x, y)
-    ax.legend(['Target F',
-               'Model',
-               'Trainset'])
-    plt.show()
+        with torch.no_grad():
+            pred = model(x_plot)
+        fig, ax = plt.subplots()
+        ax.plot(x_plot, f(x_plot))
+        ax.plot(x_plot, pred)
+        ax.scatter(x, y)
+        ax.legend(['Target F',
+                'Model',
+                'Trainset'])
+        plt.show()
