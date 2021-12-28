9c9d1c6eb12d4dd5529bb04cfc305971b9434280
damadape <danielsapien@gmail.com>
2021-12-27 23:20:44 -0600

    Notas adicionales

--- experim5.py
+++ experim5.py
@@ -29,6 +29,7 @@ load(model, path, name)
 model.eval()
 
 # Función para calcular el jacobiano para cada muestra de un batch
+# https://discuss.pytorch.org/t/computing-batch-jacobian-efficiently/80771/4
 def batch_jacobian(func, x, create_graph=False, vectorize=False):
     # x in shape (Batch, Length)
     def _func_sum(x):
@@ -50,6 +51,11 @@ x_plot = torch.linspace(x_min, x_max, 1000).view(-1,1)
 with torch.no_grad():
     pred = model(x_plot)
     pred_j = batch_jacobian(model, x_plot).squeeze()
+    # diagonal(jacobian(batch)) -> 136 ms ± 763 µs
+    # vectorize, graph -> 828 µs ± 8.27 µs
+    # vectorize -> 740 µs ± 2.78 µs
+    # graph -> 611 µs ± 3.65 µs
+    # _ -> 554 µs ± 2.17 µs
 
 fig, ax = plt.subplots()
 ax.plot(x_plot, f(x_plot))
