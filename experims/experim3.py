a30c9dabfc5e72628111e2ae9dc6196d3b04cb3f
damadape <danielsapien@gmail.com>
2022-01-15 06:07:36 -0600

    Pruebas de hiperparámetros

--- experims/experim3.py
+++ experims/experim3.py
@@ -16,13 +16,13 @@ from experimR import RoboKinSet
 from experim0 import MLP
 
 
-class MLP_PL(pl.LightningModule):
+class MLP_Regressor(pl.LightningModule):
 
-    def __init__(self, input_dim=1, output_dim=1,
+    def __init__(self, robot, n_per_q, input_dim=1, output_dim=1,
                  depth=1, mid_layer_size=10, activation=torch.tanh,
-                 optim=torch.optim.Adam, batch_size=64, ylr=1e-3):
+                 optim=torch.optim.Adam, batch_size=256, lr=1e-3):
         super().__init__()
-        self.save_hyperparameters()
+        self.save_hyperparameters()#ignore='robot'
         self.model = MLP(input_dim, output_dim, depth,
                                mid_layer_size, activation)
 
@@ -51,37 +51,6 @@ class MLP_PL(pl.LightningModule):
         self.log('val_loss', val_loss)
         return val_loss
 
-
-class FKRegressor(pl.LightningModule):
-
-    def __init__(self, model, robot, n_per_q,
-                 batch_size=64, lr=1e-3, optim=torch.optim.Adam):
-        super().__init__()
-        self.save_hyperparameters(ignore='model')
-        self.model = model
-
-    def forward(self, x):
-        return self.model(x)
-
-    def configure_optimizers(self):
-        optimizer = self.hparams.optim(self.parameters(), lr=self.hparams.lr)
-        return optimizer
-        
-    def training_step(self, batch, batch_idx):
-        point, target = batch
-        pred = self(point)
-        loss = F.mse_loss(pred, target)
-        self.log('train_loss', loss)
-        self.log('hp_metric', loss)
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        point, target = batch
-        pred = self(point)
-        val_loss = F.mse_loss(pred, target)
-        self.log('val_loss', val_loss)
-        return val_loss
-
     def train_dataloader(self):
         ns_samples = [self.hparams.n_per_q] * self.hparams.robot.n
         train_set = RoboKinSet.grid_sampling(self.hparams.robot, ns_samples)
@@ -90,8 +59,8 @@ class FKRegressor(pl.LightningModule):
 
     def val_dataloader(self):
         n_samples = self.hparams.n_per_q ** self.hparams.robot.n // 5
-        val_set = RoboKinSet(robot, n_samples)
-        loader = DataLoader(val_set, batch_size=batch_size)
+        val_set = RoboKinSet(self.hparams.robot, n_samples)
+        loader = DataLoader(val_set, batch_size=self.hparams.batch_size)
         return loader
 
 
@@ -101,18 +70,20 @@ if __name__ == '__main__':
     from pytorch_lightning.callbacks import ModelCheckpoint
     from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
+    from experim9 import FKRegressor
+
     # pl.seed_everything(36)
 
     # Hyperparams
-    depth = 10
+    depth = 3
     mid_layer_size = 10
-    activation = torch.relu
+    activation = torch.tanh
     lr = 1e-3
     batch_size = 1024
-    epochs = 50
+    epochs = 10
 
-    robot = rtb.models.DH.Cobra600()
-    n_per_q = 10
+    robot = rtb.models.DH.Orion5()
+    n_per_q = 12
 
     input_dim = robot.n
     output_dim = 3
@@ -120,32 +91,34 @@ if __name__ == '__main__':
     """
     Entrenamiento
     """
-    logger = TensorBoardLogger('lightning_logs', 'exp3_1',
+    logger = TensorBoardLogger('lightning_logs', 'exp3',
                                log_graph=True)
 
-    # model = MLP_PL(input_dim, output_dim,
-    #                depth, mid_layer_size,
-    #                activation)
+    # regressor = MLP(input_dim=input_dim, output_dim=output_dim,
+    #                 depth=depth, mid_layer_size=mid_layer_size,
+    #                 activation=activation)
+    # model = FKRegressor(regressor, robot, n_per_q, batch_size, lr)
 
-    model = MLP(input_dim, output_dim,
-                depth, mid_layer_size,
-                activation)
-
-    regressor = FKRegressor(model, robot, n_per_q, batch_size, lr)
+    model = MLP_Regressor(robot, n_per_q,
+                          input_dim=input_dim, output_dim=output_dim,
+                          depth=depth, mid_layer_size=mid_layer_size,
+                          activation=activation, batch_size=batch_size, lr=lr)
 
     # Callbacks de entrenamiento
-    # checkpoint_cb = ModelCheckpoint(monitor='val_loss',
-    #                                 dirpath='modelos/experim3/')
-    #                                 #save_top_k=
+    # train_checkpoint = ModelCheckpoint()
+        
+    # last_checkpoint = ModelCheckpoint(monitor='val_loss',
+    #                                   dirpath='models/experim3/')
+    #                                   #save_top_k=
 
     early_stop_cb = EarlyStopping(monitor="val_loss")
 
-    # trainer = pl.Trainer(fast_dev_run=True)
+    # trainer = pl.Trainer(fast_dev_run=True, logger=logger)
     trainer = pl.Trainer(max_epochs=epochs,
                          logger=logger,
-                         auto_lr_find=True,
-                         auto_scale_batch_size=True,
+    #                      auto_lr_find=True,
+    #                      auto_scale_batch_size=True,
                          callbacks=[#checkpoint_cb,
                                     early_stop_cb])
-    trainer.tune(regressor)
-    trainer.fit(regressor)
+    #trainer.tune(model)
+    trainer.fit(model)
