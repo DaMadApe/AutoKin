6c803dc8a2fcaa0bd8a9f5ce4a1556f7d005c072
damadape <danielsapien@gmail.com>
2022-01-23 04:53:19 -0600

    Stub de inicio

--- experims/experim12.py
+++ experims/experim12.py
@@ -1,4 +1,21 @@
 """
-Aprendizaje activo: Selección dinámica de puntos
-que favorezcan la convergencia del entrenamiento.
+Aprendizaje activo: Selección dinámica de puntos para
+mejorar la convergencia de la regresión de experim0
 """
+
+import modAL
+
+from experim0 import MLP
+from experim14 import train
+
+
+if __name__ == "__main__":
+    import torch
+    from torch.utils.data import TensorDataset, DataLoader
+    from modAL.models import ActiveLearner
+
+    x_min = -1
+    x_max = 1
+
+    #def f(x): return 3*x**3 - 3*x**2 + 2*x
+    def f(x): return torch.sin(10*x**2 - 10*x)
