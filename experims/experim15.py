fd9e616a8ca2123ebde41c6d657e50e6824098b8
damadape <danielsapien@gmail.com>
2022-01-23 04:52:34 -0600

    Enunciado

---
+++ experims/experim15.py
@@ -0,0 +1,8 @@
+"""
+Obtención numérica de un Jacobiano.
+- Usar trayectorias de experim11
+- Diferenciación manual de los puntos adyacentes
+
+Prox Exp: Primeras pruebas de compensación de error dinámico
+"""
+
