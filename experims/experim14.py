cb1bb5ff25eee91dda3687377bf66873e8cf3718
damadape <danielsapien@gmail.com>
2022-01-25 23:47:29 -0600

    Intento de lr scheduler

--- experims/experim14.py
+++ experims/experim14.py
@@ -3,15 +3,18 @@ Métodos de entrenamiento múltiple con pytorch puro.
 """
 import torch
 import torch.nn as nn
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 #from torch.utils.tensorboard import SummaryWriter
 
 from tqdm import tqdm
 
 
 def train(model, train_loader, val_loader=None, epochs=10,
-          lr=1e-3, criterion=nn.MSELoss(), optim=torch.optim.Adam):
+          lr=1e-3, criterion=nn.MSELoss(), optim=torch.optim.Adam,
+          lr_scheduler=False):
     optimizer = optim(model.parameters(), lr=lr)
     progress = tqdm(range(epochs), desc='Training')
+    scheduler = ReduceLROnPlateau(optimizer)#, patience=5)
     for _ in progress:
         # Train step
         # Invertir muestras para fines de exp
@@ -30,7 +33,11 @@ def train(model, train_loader, val_loader=None, epochs=10,
                     pred = model(X)
                     val_loss = criterion(pred, Y)
 
-        progress.set_postfix(Loss=loss.item(), Val=val_loss.item())
+                    if lr_scheduler:
+                        scheduler.step(val_loss)
+            progress.set_postfix(Loss=loss.item(), Val=val_loss.item())
+        else:
+            progress.set_postfix(Loss=loss.item())
 
 
 if __name__ == "__main__":
@@ -46,7 +53,7 @@ if __name__ == "__main__":
     """
     Conjunto de datos
     """
-    n_per_q = 10
+    n_per_q = 6
     n_samples = n_per_q ** robot.n
 
     ns_samples = [n_per_q] * robot.n
@@ -65,7 +72,8 @@ if __name__ == "__main__":
                 depth=4, mid_layer_size=10,
                 activation=torch.tanh)
 
-    train(model, train_loader, val_loader, epochs=100)
+    train(model, train_loader, val_loader, epochs=1000,
+          lr=1e-3, lr_scheduler=False)
 
-    path = 'models/experim14/v1.pt'
+    path = 'models/experim14_v1.pt'
     torch.save(model, path)
