060315022fb5798e764547d57f88abde984d7476
damadape <danielsapien@gmail.com>
2022-02-06 05:23:07 -0600

    Simplificar código

--- experims/experim0.py
+++ experims/experim0.py
@@ -4,10 +4,6 @@ Pruebas de regresión  con pytorch puros
 import os
 import torch
 from torch import nn
-from torch.nn import functional as F
-from torch.utils.data import TensorDataset, DataLoader
-import matplotlib.pyplot as plt
-from tqdm import tqdm
 
 """
 MLP configurable de ancho constante
@@ -38,44 +34,18 @@ class MLP(nn.Module):
         x = self.layers[-1](x)
         return x
 
-"""
-Save:
-torch.save(model, PATH)
-
-Load:
-# Model class must be defined somewhere
-model = torch.load(PATH)
-model.eval()
-"""
-def save(model, path, name):
-    name += ".pt"
-    if not os.path.exists(path):
-        print("Path doesn't exist, creating folder...")
-        os.makedirs(path)
-    save_path = os.path.join(path, name)
-    torch.save(model.state_dict(), save_path)
-
-def load(model, path, name):
-    name += ".pt"
-    load_path = os.path.join(path, name)
-    model.load_state_dict(torch.load(load_path))
-
 
 if __name__ == '__main__':
 
-    #torch.manual_seed(42)
+    from torch.utils.data import TensorDataset
+    import matplotlib.pyplot as plt
+
+    from experim14 import train
+
+    torch.manual_seed(42)
 
     # args
-    lr = 5e-3
-    depth = 6
-    mid_layer_size = 10
-    activation = torch.tanh
     n_samples = 32
-    batch_size = 32
-    epochs = 2000
-
-    input_dim = 1
-    output_dim = 1
     x_min = -1
     x_max = 1
     view_plot = True
@@ -86,35 +56,24 @@ if __name__ == '__main__':
     """
     Conjunto de datos
     """
-    x = torch.linspace(x_min, x_max, n_samples).view(-1, input_dim)
+    x = torch.linspace(x_min, x_max, n_samples).view(-1, 1)
     y = f(x)
 
     train_set = TensorDataset(x, y)
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-
 
     """
     Entrenamiento
     """
-    model = MLP(input_dim, output_dim,
-                depth, mid_layer_size,
-                activation)
+    model = MLP(input_dim=1, output_dim=1,
+                depth=6,
+                mid_layer_size=10,
+                activation=torch.tanh)
 
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-
-    progress = tqdm(range(epochs), desc='Training')
-    for _ in progress:
-        # Train step
-        for X, Y in train_loader:
-            model.train()
-            pred = model(X)
-            loss = criterion(pred, Y)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-        progress.set_postfix(Loss=loss.item())
+    train(model, train_set,
+          epochs=2000,
+          lr=5e-3,
+          batch_size=32,
+          log_dir='tb_logs/')
 
     """
     Visualización de datos en una dimensión
@@ -134,8 +93,13 @@ if __name__ == '__main__':
         plt.show()
 
     # Guardar modelo
+    """
+    Save:
+    torch.save(model, PATH)
+
+    Load:
+    # Model class must be defined somewhere
+    model = torch.load(PATH)
+    model.eval()
+    """
     torch.save(model, 'models/experim0_v1.pt')
-    
-    # Cargar modelo
-    # model = torch.load(PATH)
-    # model.eval()
