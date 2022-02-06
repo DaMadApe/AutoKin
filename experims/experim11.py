cbd44ea6b9f1a9a1aa7d4dfc4a8e9a4afed91163
damadape <danielsapien@gmail.com>
2022-02-06 06:11:36 -0600

    Comentarios

--- experims/experim11.py
+++ experims/experim11.py
@@ -1,7 +1,7 @@
 """
 Pruebas de trayectorias para explorar el espacio de parámetros
 """
-import numpy as np
+import numpy as np # TODO: Revisar si cambiar por torch
 
 def n_hilbert(n_dim, n_points, res=1):
     """
@@ -11,36 +11,30 @@ def n_hilbert(n_dim, n_points, res=1):
     return np.zeros(n_points, n_dim)    
 
 
-def coprime_sines(n_dim, n_points):
-    #https://www.desmos.com/calculator/m4pjhqjgz6
-    coefs = [11, 13, 17, 19, 23, 29, 31]
+def coprime_sines(n_dim, n_points, wiggle=0):
+    # https://www.desmos.com/calculator/m4pjhqjgz6
+    """
+    El muestreo de estas curvas suele concentrarse en los
+    límites del espacio, y pasa por múltiples
+    coordenadas con valor 0, por lo que podría atinarle a
+    las singularidades de un robot si se usan las curvas
+    en el espacio de parámetros.
+    """
+    coefs = [5, 7, 11, 13, 17, 19, 23, 29, 31]
     coefs = np.array(coefs) * 2*np.pi
     points = np.stack([np.linspace(0, 1, n_points)] * n_dim)
     for i in range(n_dim):
-        points[i] = np.sin(coefs[i]*points[i])
+        points[i] = np.sin(coefs[i+wiggle]*points[i])
     return points
+
+# Derivada de coprime_sines
+def coprime_sines_dif(n_dim, n_points, wiggle=0):
+    pass
 
 
 if __name__ == "__main__":
 
     import matplotlib.pyplot as plt
-    """
-    import roboticstoolbox as rtb
-    
-    robot = rtb.models.DH.Cobra600()
-
-    n_points = 5
-
-    qs = np.zeros(n_points, robot.n)
-    ps = robot.fkine(qs)
-    j_samples = []
-
-    j_real = [robot.jacob0(q)[] for q in qs]
-
-    for i in range(n_points - 1):
-        dq = qs[i] - qs[i+1]
-        dp = ps[i] - ps[i+1]
-        j_samples.append(dp/dq) """
 
     curve = coprime_sines(2, 300)
     plt.scatter(*curve)
