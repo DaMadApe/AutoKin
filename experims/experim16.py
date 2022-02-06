9c00df332f968a19b1d93062f1d2790c8c2cee93
damadape <danielsapien@gmail.com>
2022-02-06 06:10:43 -0600

    Pruebas de ActiveLearn propio

--- experims/experim16.py
+++ experims/experim16.py
@@ -1,8 +1,12 @@
 """
 Réplica de experim 12 implementado desde cero (sin modAL)
+Evitar numpy, usar torch donde sea posible.
 """
-
 import torch
+from torch.utils.data import TensorDataset
+
+from experim14 import train
+
 
 class EnsembleRegressor(torch.nn.Module):
     """
@@ -12,10 +16,16 @@ class EnsembleRegressor(torch.nn.Module):
         super().__init__()
         self.ensemble = torch.nn.ModuleList(models)
     
-    def forward (self, x):
+    def forward(self, x):
         return torch.stack([model(x) for model in self.ensemble])
 
-    def query(self, candidate_batch):
+    def predict(self, x):
+        with torch.no_grad():
+            self.ensemble.eval()
+            preds = self(x)
+        return torch.mean(preds, dim=0)
+
+    def query(self, candidate_batch, n_queries=1):
         """
         De un conjunto de posibles puntos, devuelve
         el punto que maximiza la desviación estándar
@@ -37,9 +47,8 @@ class EnsembleRegressor(torch.nn.Module):
         # varianza entre modelos de cada coordenada.
         # https://math.stackexchange.com/questions/850228/finding-how-spreaded-a-point-cloud-in-3d
         deviation = torch.sum(torch.var(preds, axis=0), axis=-1)
-        candidate_idx = torch.argmax(deviation)
+        candidate_idx = torch.topk(deviation, n_queries).indices
         query = candidate_batch[candidate_idx]
-
         return candidate_idx, query
         # return torch.topk(deviation, n_queries)
 
@@ -49,8 +58,11 @@ class EnsembleRegressor(torch.nn.Module):
             train(model, train_set, **train_kwargs)
 
 
-class MockModule(torch.nn.Module):
 
+class MockModule(torch.nn.Module):
+    """
+    Módulo auxiliar para probar ensamble
+    """
     def __init__(self):
         super().__init__()
 
@@ -59,12 +71,51 @@ class MockModule(torch.nn.Module):
 
 
 if __name__ == "__main__":
-    
+
+    import matplotlib.pyplot as plt
+
     from experim0 import MLP
 
-    ensemble = EnsembleRegressor([MockModule() for _ in range(5)])
+    x_min = -1
+    x_max = 1
+    n_samples = 30
+    n_models = 5
+    n_queries = 10
 
-    qs = torch.rand(4, 6)
+    #def f(x): return 3*x**3 - 3*x**2 + 2*x
+    def f(x): return torch.sin(10*x**2 - 10*x)
 
-    print(ensemble(qs))
-    print(ensemble.query(qs))
+    X_train = torch.linspace(x_min, x_max, n_samples).view(-1, 1)
+    y_train = f(X_train)
+
+    X_query = torch.linspace(x_min/2, x_max, 100).reshape(-1, 1)/1.2
+
+    models = [MLP(input_dim=1,
+                  output_dim=1,
+                  depth=3,
+                  mid_layer_size=10,
+                  activation=torch.tanh) for _ in range(n_models)]
+
+    ensemble = EnsembleRegressor(models)
+
+    # Entrenar el model con datos iniciales
+    train_set = TensorDataset(X_train, y_train)
+    ensemble.fit(train_set, epochs=100)
+    # Solicitar recomenación de nuevas muestras
+    _, queries = ensemble.query(X_query, n_queries)
+
+    """
+    Graficar conjunto de datos
+    """
+    X_plot = torch.linspace(x_min, x_max, 1000).view(-1,1)
+
+    fig, ax = plt.subplots()
+    ax.plot(X_plot, f(X_plot))
+    ax.scatter(X_train, y_train)
+    ax.scatter(queries, f(queries))
+    ax.plot(X_plot, ensemble.predict(X_plot))
+    ax.legend(['Target F',
+               'Trainset',
+               'Queries',
+               'Model'])
+    plt.show()
