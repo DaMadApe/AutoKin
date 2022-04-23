9f152c428fd029e6889b15f779d44b0d74f0e44f
damadape <danielsapien@gmail.com>
2022-04-23 07:34:04 -0500

    setup_logging devuelve logger para uso externo

--- experimentos/experim.py
+++ experimentos/experim.py
@@ -6,47 +6,55 @@ import inspect
 
 import torch
 
-def setup_logging():
-    filename = inspect.stack()[2].filename
+def setup_logging(exec_call=False):
+    filename = inspect.stack()[1+exec_call].filename
     filename = os.path.splitext(os.path.basename(filename))[0]
     timestamp = time.strftime('%Y%m%d-%H%M%S')
     file_dir=f'experimentos/logs/{filename}_{timestamp}.log'
 
     file_handler = logging.FileHandler(file_dir)
     stream_handler = logging.StreamHandler(sys.stdout)
-
     stream_handler.setLevel(logging.INFO)
 
-    logging.basicConfig(format='%(message)s',
-                        handlers=[file_handler, stream_handler],
-                        level=logging.DEBUG)
+    logger = logging.getLogger('experim')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
 
-    return logging.getLogger()
+    # logging.basicConfig(format='%(message)s',
+    #                     handlers=[file_handler, stream_handler],
+    #                     level=logging.DEBUG)
+
+    return logger
 
 
 def ejecutar_experimento(n_reps, experimento, *exp_args,
                          model_save_dir=None, log_all_products=True,
+                         anotar=True,
                          **exp_kwargs):
 
-    setup_logging()
+    logger = logging.getLogger('experim')
+    if not logger.hasHandlers():
+        logger = setup_logging(exec_call=True)
 
     best_score = torch.inf
 
     for _ in range(n_reps):
         score, product = experimento(*exp_args, **exp_kwargs)
         if log_all_products:
-            logging.info(f'Producto: {product}')
-        logging.info(f'Puntaje: {score}')
+            logger.info(f'Producto: {product}')
+        logger.info(f'Puntaje: {score}')
 
         if score < best_score:
             best_score = score
             best_product = product
 
-    logging.info(f'Mejor producto: {best_product}')
-    logging.info(f'Mejor puntaje = {best_score}')
+    logger.info(f'Mejor producto: {best_product}')
+    logger.info(f'Mejor puntaje = {best_score}')
 
-    anotaciones = input('Anotaciones:\n')
-    logging.debug('Anotaciones:\n' + anotaciones)
+    if anotar:
+        anotaciones = input('Anotaciones:\n')
+        logger.debug('Anotaciones:\n' + anotaciones)
 
     if model_save_dir is not None:
         torch.save(product, model_save_dir)
