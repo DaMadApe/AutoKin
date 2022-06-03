9392dcaf3d1d8058ea4646124eb189a82e672d80
damadape <danielsapien@gmail.com>
2022-06-03 05:51:25 -0500

    Agregar out_n y opcion de pose, config de sofa,etc

--- robot.py
+++ robot.py
@@ -1,13 +1,15 @@
+from multiprocessing.sharedctypes import Value
 import torch
 from torch.autograd.functional import jacobian
 
 import roboticstoolbox as rtb
 
+from robot_mixins import IkineMixin
 from utils import random_robot
-from ikine import ikine_pi_jacob
+from sofa.sofa_call import sofa_fkine
 
 
-class Robot():
+class Robot(IkineMixin):
     """
     Clase base para definir un robot que interactúe con
     el resto del programa.
@@ -15,8 +17,9 @@ class Robot():
     rango [0, 1], por lo que se debe hacer normalización
     si se enlaza con un robot que opere con otros valores
     """
-    def __init__(self, n):
-        self.n = n # Número de actuadores
+    def __init__(self, n_act, out_n):
+        self.n = n_act # Número de actuadores
+        self.out_n = out_n # Número de salidas
 
     def fkine(self, q: torch.Tensor)->tuple[torch.Tensor, torch.Tensor]:
         """
@@ -26,9 +29,7 @@ class Robot():
         """
         raise NotImplementedError
 
-    # def jacobian(self, q) ?
-
-    def ikine(self, p): # Hacer método de Robot?
+    def jacob(self, q):
         raise NotImplementedError
 
 
@@ -40,57 +41,62 @@ class RTBrobot(Robot):
     Para usar otra librería robótica con el resto del
     programa, se debe definir una interfaz como esta.
     """
-    def __init__(self, robot):
-        super().__init__(robot.n)
+    def __init__(self, robot, full_pose=False):
         self.robot = robot
+        self.full_pose = full_pose
+        super().__init__(robot.n, out_n=6 if full_pose else 3)
 
     @classmethod
-    def from_name(cls, name):
+    def from_name(cls, name, full_pose=False):
         robot = getattr(rtb.models.DH, name)()
-        return cls(robot)
+        return cls(robot, full_pose)
 
     @classmethod
-    def random(cls, *args, **kwargs):
-        return cls(random_robot(*args, **kwargs))
+    def random(cls, full_pose=False, *args, **kwargs):
+        return cls(random_robot(*args, **kwargs), full_pose)
 
     def __repr__(self):
         return self.robot.__repr__()
 
     def fkine(self, q):
         denormed_q = self.denorm(q)
-        p = self.robot.fkine(denormed_q.detach().numpy()).t
-        p = torch.tensor(p, dtype=torch.float)
+        se3_pose = self.robot.fkine(denormed_q.detach().numpy())
+
+        if self.full_pose:
+            pos = torch.tensor(se3_pose.t)
+            ori = torch.tensor(se3_pose.eul())
+            p = torch.cat((pos, ori), dim=1) # Juntar pos y rpy en un vector de 6 elems
+        else:
+            p = torch.tensor(se3_pose.t) # Extraer componente de traslación
+
+        p = p.float()
         return q, p
 
-    def jacobian(self, q):
-        return torch.tensor(self.robot.jacob0(q.numpy())[:3]).float()
+    def jacob(self, q):
+        J = self.robot.jacob0(q.numpy())
 
-    def ikine(self, q_start, p_target):
-        return ikine_pi_jacob(q_start, p_target, eta=0.1,
-                              fkine=self.fkine, 
-                              jacob=self.jacobian)
+        if not self.full_pose:
+            J = J[:3]
+
+        return torch.tensor(J).float()
 
     def denorm(self, q):
         q_min, q_max = torch.tensor(self.robot.qlim, dtype=torch.float32)
         return q * (q_max - q_min) + q_min
 
 
-class RTBnonLinear(Robot):
+class SofaRobot(Robot):
 
-    def __init__(self, n_joints):
-        self.virtual_bot = RTBrobot.random(min_n=n_joints*2, max_n=n_joints*4)
+    def __init__(self, config='LSL'):
+        if config not in ['LLLL','LSLS','LSSL', 'LSL', 'SLS', 'LLL', 'LS', 'LL']:
+            raise ValueError('Configuración inválida')
+        self.config = config
+        super().__init__(n_act=len(config), out_n=3)
 
-    def q_trans(self, q):
-        trans_q = q
-        return trans_q
-
-    def fkine(self, q):
-        trans_q = self.q_trans(q)
-        return self.virtual_bot.fkine(trans_q)
-
-    def jacobian(self, q):
-        trans_q = self.q_trans(q)
-        return self.virtual_bot.jacobian(trans_q)
+    def fkine(self, q, headless=True):
+        p = sofa_fkine(q.numpy(), headless=headless, config=self.config)
+        p = torch.tensor(p, dtype=torch.float)
+        return q, p
 
 
 class ExternRobot(Robot):
@@ -123,7 +129,7 @@ class ModelRobot(Robot):
     """
     def __init__(self, model):
         self.model = model
-        super().__init__(model.input_dim)
+        super().__init__(model.input_dim, model.output_dim)
 
     @classmethod
     def load(cls, model_dir):
@@ -134,5 +140,5 @@ class ModelRobot(Robot):
     def fkine(self, q):
         return self.model(q)
 
-    def jacobian(self, q):
+    def jacob(self, q):
         return jacobian(self.model, q).squeeze()
