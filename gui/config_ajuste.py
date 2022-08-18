00767d0f7f04ffc6e2d6b3a1fa14c7caafcc53cc
damadape <danielsapien@gmail.com>
2022-08-18 05:34:14 -0500

    Agregar checkboxes y tabs para cada etapa

--- gui/config_ajuste.py
+++ gui/config_ajuste.py
@@ -7,28 +7,51 @@ from gui.gui_utils import Label_Entry
 
 class PantallaConfigAjuste(ttk.Frame):
 
-    train_args = {
-        'epochs': {
-            'label': '# de épocas',
-            'var_type': 'int',
-            'default_val': 1000,
-            'restr_positiv': True,
-            'non_zero': True
+    args_etapas = {
+        'Meta ajuste': {
+
         },
-        'lr': {
-            'label': 'learning rate',
-            'var_type': 'float',
-            'default_val': 1e-3,
-            'restr_positiv': True,
-            'non_zero': True
+
+        'Ajuste inicial': {
+            'epochs': {
+                'label': '# de épocas',
+                'var_type': 'int',
+                'default_val': 1000,
+                'restr_positiv': True,
+                'non_zero': True
+            },
+            'lr': {
+                'label': 'learning rate',
+                'var_type': 'float',
+                'default_val': 1e-3,
+                'restr_positiv': True,
+                'non_zero': True
+            },
+            'batch_size': {
+                'label': 'batch size',
+                'var_type': 'int',
+                'default_val': 256,
+                'restr_positiv': True,
+                'non_zero': True
+            }
         },
-        'batch_size': {
-            'label': 'batch size',
-            'var_type': 'int',
-            'default_val': 256,
-            'restr_positiv': True,
-            'non_zero': True
-        },
+
+        'Ajuste dirigido': {
+            'query_steps': {
+                'label': 'Pasos de muestreo',
+                'var_type': 'int',
+                'default_val': 5,
+                'restr_positiv': True,
+                'non_zero': True
+            },
+            'n_queries': {
+                'label': 'Muestras por paso',
+                'var_type': 'int',
+                'default_val': 5,
+                'restr_positiv': True,
+                'non_zero': True
+            }
+        }
     }
 
     def __init__(self, parent):
@@ -40,36 +63,64 @@ class PantallaConfigAjuste(ttk.Frame):
         self.parent.rowconfigure(0, weight=1)
         self.parent.title("Configurar aproximación")
 
-        self.arg_getters = {}
+        self.arg_getters = {etapa: {} for etapa in self.args_etapas.keys()}
 
         self.controlador = UIController()
 
         self.definir_elementos()
 
     def definir_elementos(self):
-        # Parámetros de ajuste
-        frame_t_params = ttk.LabelFrame(self, text='Parámetros de ajuste')
-        frame_t_params.grid(column=0, row=1, columnspan=2, sticky='nsew')
+        # Checkboxes para etapas de entrenamiento
+        frame_checks = ttk.Frame(self)
+        frame_checks.grid(column=0, row=0, sticky='nsew')
 
-        for i, (arg_name, entry_kwargs) in enumerate(self.train_args.items()):
-            entry = Label_Entry(frame_t_params, **entry_kwargs)
-            entry.grid(column=0, row=i)
-            self.arg_getters[arg_name] = entry.get
+        titulo_checks = ttk.Label(frame_checks,
+                                  text="Seleccionar etapas de ajuste",
+                                  font=(12))
+        titulo_checks.grid(column=0, row=0, padx=5, pady=5)
+
+        self.checks = {}
+        for i, etapa in enumerate(self.args_etapas.keys()):
+            check_but = ttk.Checkbutton(frame_checks, text=etapa)
+            check_but.grid(column=0, row=i+1,
+                           padx=5, pady=5, sticky='w')
+            self.checks[etapa] = check_but
+
+        # Configuración de cada etapa
+        tabs_config = ttk.Notebook(self)
+        tabs_config.grid(column=0, row=1, sticky='nsew')
+
+        for etapa, args in self.args_etapas.items():
+            frame = ttk.Frame(tabs_config)
+
+            for i, (arg_name, entry_kwargs) in enumerate(args.items()):
+                entry = Label_Entry(frame, **entry_kwargs)
+                entry.grid(column=0, row=i, padx=5, pady=5)
+                self.arg_getters[etapa][arg_name] = entry.get
+
+            tabs_config.add(frame, text=etapa)
+
+        # Comportamiento default: Check de a. inicial bloqueado en True
+        #                         Tab de a. inicial por default
+        #                         Desactivar tabs no usadas
 
         # Botones inferiores
-        boton_cancelar = ttk.Button(self, text="Cancelar",
-                                   command=self.parent.regresar)
-        boton_cancelar.grid(column=0, row=2, sticky='w')
+        frame_botones = ttk.Frame(self)
+        frame_botones.grid(column=0, row=2, sticky='we')
+        frame_botones.columnconfigure(0, weight=1)
 
-        boton_cancelar = ttk.Button(self, text="Ejecutar",
+        boton_cancelar = ttk.Button(frame_botones,
+                                    text="Cancelar",
+                                    command=self.parent.regresar)
+        boton_cancelar.grid(column=0, row=0, sticky='w')
+
+        boton_ejecutar = ttk.Button(frame_botones,
+                                    text="Ejecutar",
                                     command=self.ejecutar)
-        boton_cancelar.grid(column=1, row=2, sticky='e')
+        boton_ejecutar.grid(column=1, row=0, sticky='e')
 
         for child in self.winfo_children():
             child.grid_configure(padx=10, pady=5)
-
-        for child in frame_t_params.winfo_children():
-            child.grid_configure(padx=5, pady=5)
 
         # Comportamiento al cambiar de tamaño
         self.columnconfigure(0, weight=1)
