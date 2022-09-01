9b02472835b51b3d361470b0bc30043442be1d70
damadape <danielsapien@gmail.com>
2022-09-01 08:58:54 -0500

    Agregar componentes base

--- gui/config_muestreo.py
+++ gui/config_muestreo.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
 
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+from gui.robot_database import UIController
+
 
 class PantallaConfigMuestreo(ttk.Frame):
 
@@ -13,9 +18,28 @@ class PantallaConfigMuestreo(ttk.Frame):
         self.parent.rowconfigure(0, weight=1)
         self.parent.title("Configurar muestreo")
 
+        self.controlador = UIController()
+
         self.definir_elementos()
 
     def definir_elementos(self):
+
+        # Frame lista + gráfica + opciones de proyección
+        frame_grafica = ttk.Frame(self)
+        frame_grafica.grid(column=0, row=0)
+
+        # Gráfica
+        # self.fig = Figure(figsize=(8,8), dpi=90)
+        # self.ax = self.fig.add_subplot(projection='3d')
+        # self.fig.tight_layout()
+        # self.grafica = FigureCanvasTkAgg(self.fig, master=frame_grafica)
+        # self.grafica.get_tk_widget().grid(column=1, row=0, sticky='n',
+        #                                   rowspan=3, padx=5, pady=5)
+        # self.recargar_grafica()
+
+        # Frame configs
+
+        # Agregar config de split train-val-test
 
         # Botones
         boton_regresar = ttk.Button(self, text="Regresar",
@@ -25,3 +49,16 @@ class PantallaConfigMuestreo(ttk.Frame):
         boton_aceptar = ttk.Button(self, text="Aceptar",
             command=self.parent.avanzar)
         boton_aceptar.grid(column=1, row=2, sticky='e')
+
+
+    def recargar_grafica(self):
+        self.ax.clear()
+        if self.puntos:
+            puntosTranspuesto = list(zip(*self.puntos))
+            self.ax.plot(*puntosTranspuesto[:3], color='lightcoral',
+                        linewidth=1.5)
+            self.ax.scatter(*puntosTranspuesto[:3], color='red')
+        self.ax.set_xlabel('q1')
+        self.ax.set_ylabel('q2')
+        self.ax.set_zlabel('q3')
+        self.grafica.draw()
