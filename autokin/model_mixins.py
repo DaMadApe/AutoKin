bb626eff02dfb58fb5a23ffbebd3c955fb38b6b7
damadape <danielsapien@gmail.com>
2022-10-12 13:39:38 -0500

    Agregar mixin para ensemble

--- autokin/model_mixins.py
+++ autokin/model_mixins.py
@@ -2,10 +2,14 @@ import inspect
 
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
+from torch.utils.data import (Dataset, DataLoader,
+                              TensorDataset, ConcatDataset,
+                              random_split)
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from autokin.loggers import Logger, TqdmDisplay, TBLogger
+from autokin.robot import RTBrobot
+from autokin.muestreo import FKset
 
 
 class HparamsMixin:
@@ -229,3 +233,125 @@ class DataFitMixin:
                 test_loss = criterion(pred, Y)
 
         return test_loss
+
+
+class EnsembleMixin:
+    def __init__(self):
+        super().__init__()
+        self.best_model_idx = None
+        self.model_scores = None
+    
+    def __getitem__(self, idx):
+        """
+        Facilitar acceso a los modelos individuales desde afuera
+        """
+        return self.ensemble[idx]
+    
+    def forward(self, x):
+        """
+        Este forward (tal vez) no tiene utilidad para entrenamiento
+        """
+        return torch.stack([model(x) for model in self.ensemble])
+
+    def joint_predict(self, x):
+        """
+        Devuelve la predicción promedio de todos los modelos
+        """
+        with torch.no_grad():
+            self.ensemble.eval()
+            preds = self(x)
+        return torch.mean(preds, dim=0)
+
+    def best_predict(self, x):
+        """
+        Devuelve la predicción del mejor modelo
+        (requiere ejecutar antes self.test con un set de prueba)
+        """
+        if self.best_model_idx is None:
+            raise RuntimeError('No se hizo ensemble.test()')
+        else:
+            with torch.no_grad():
+                self.ensemble[self.best_model_idx].eval()
+                pred = self.ensemble[self.best_model_idx](x)
+            return pred
+
+    def query(self, candidate_batch=None, n_queries=1):
+        """
+        De un conjunto de posibles puntos, devuelve
+        el punto que maximiza la desviación estándar
+        de las predicciones del grupo de modelos.
+        """
+        if candidate_batch is None:
+            candidate_batch = torch.rand((1000, self.input_dim))
+
+        with torch.no_grad():
+            self.ensemble.eval()
+            preds = self(candidate_batch)
+
+        # La desviación estándar de cada muestra es la suma de la
+        # varianza entre modelos de cada coordenada.
+        # https://math.stackexchange.com/questions/850228/finding-how-spreaded-a-point-cloud-in-3d
+        deviation = torch.sum(torch.var(preds, axis=0), axis=-1)
+        candidate_idx = torch.topk(deviation, n_queries).indices
+        query = candidate_batch[candidate_idx]
+        return query
+        # return torch.topk(deviation, n_queries)
+
+    def meta_fit(self, **m_fit_kwargs):
+        """
+        Entrenar cada uno de los modelos individualmente
+        """
+        for model in self.ensemble:
+            model.meta_fit(**m_fit_kwargs)
+
+    def fit(self, train_set, **train_kwargs):
+        """
+        Entrenar cada uno de los modelos individualmente
+        """
+        for model in self.ensemble:
+            model.fit(train_set, **train_kwargs)
+
+    def test(self, test_set, **test_kwargs):
+        self.model_scores = []
+        for model in self.ensemble:
+            score = model.test(test_set, **test_kwargs)
+            self.model_scores.append(score)
+
+        self.best_model_idx = self.model_scores.index(min(self.model_scores))
+
+        return self.model_scores.copy()
+
+    def active_fit(self, train_set, label_fun,
+                   query_steps : int,
+                   n_queries: int =1,
+                   candidate_batch = None,
+                   ext_interrupt=None,
+                   **train_kwargs):
+        """
+        Ciclo para solicitar muestras y ajustar a ellas
+
+        args:
+        train_set (Dataset) : Conjunto base de entrenamiento
+        label_fun (Callable: Tensor(N,d)->Tensor(N,d)) : Método para obtener las 
+            etiquetas de nuevas muestras
+        query_steps (int) : Número de veces que se solicitan nuevas muestras
+        n_queries (int) : Número de muestras solicitadas en cada paso
+        relative_weight (int) : Ponderación extra de las muestras nuevas (repetir en dataset)
+        tb_dir (str) : Directorio base para guardar logs de tensorboard de entrenamientos
+        **train_kwargs: Argumentos de entrenamiento usados para cada ajuste
+        """
+        for _ in range(query_steps):
+
+            query = self.query(candidate_batch=candidate_batch,
+                               n_queries=n_queries)
+
+            labels = label_fun(query)
+            new_queries = TensorDataset(query, labels)
+            train_set = ConcatDataset([train_set, new_queries])
+
+            self.fit(train_set,
+                     ext_interrupt=ext_interrupt,
+                     **train_kwargs)
+
+            if ext_interrupt is not None and ext_interrupt():
+                return
