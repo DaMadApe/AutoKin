992c3ed9877289976daa70a949cc749c41e28b01
damadape <danielsapien@gmail.com>
2022-11-09 09:48:11 -0600

    Integrar función de control_motor a client

--- ext_robot/client.py
+++ ext_robot/client.py
@@ -1,12 +1,7 @@
-from ext_robot.control_motor import MCUServer
+import serial
+
 from ext_robot.NatNetClient import NatNetClient
-
-
-def suavizar(q):
-    return q
-
-def alinear_datos(q, p):
-    return q, p
+from autokin.utils import suavizar, alinear_datos
 
 
 class ExtInstance:
@@ -15,16 +10,18 @@ class ExtInstance:
     y leer datos de posición enviados por las cámaras.
     """
     def __init__(self):
-        self.mcu_server = MCUServer()
-        self.mcu_server.connect()
+        try:
+            self.serialESP = serial.Serial("COM5",115200)
+        except serial.serialutil.SerialException:
+            self.serialESP = None
 
         self.cam_client = NatNetClient()
         self.cam_client.newFrameListener = self.recv_frame
         self.cam_client.rigidBodyListener = self.recv_body
         self.cam_client.run() # En el mismo lugar que inicia SofaInstance
 
-        self.units = self.send_command_str('UnitsToMillimeters')
-        self.frame_rate = self.send_command_str('FrameRate')
+        self.units = self.send_cam_command('UnitsToMillimeters')
+        self.frame_rate = self.send_cam_command('FrameRate')
 
         self.in_exec = False # Inecesario si se puede iniciar/detener stream
         self.p_stack = []
@@ -42,24 +39,44 @@ class ExtInstance:
         if self.in_exec:
             self.p_stack.append(position)
 
-    def send_command_str(self, command_str):
-        self.cam_client.sendCommand(command=self.cam_client.NAT_MESSAGESTRING,
+    def send_cam_command(self, command_str):
+        self.cam_client.sendCommand(command=self.cam_client.NAT_REQUEST,
                                     commandStr=command_str,
                                     # Modificar NatNetClient o crear fun
                                     # diferente para facilitar esta llamada
-                                    socket=self.cam_client.comm_socket,
+                                    socket=self.cam_client.commandSocket,
                                     address=(self.cam_client.serverIPAddress,
                                             self.cam_client.commandPort))
 
+    def send_q_esp(self, q): # -> ack # Saber cuando termina ejecución
+        pasos = [0]*4
+        for i, val in enumerate(q):
+            pasos[i] = str(int(val))
+        msg_pasos = ','.join(pasos)
+        print(msg_pasos)
+        self.serialESP.write(msg_pasos.encode('ascii'))
+
+    def wait_exec_esp(self):
+        pass
+
     def fkine(self, q):
-        self.send_command_str('StartRecording')
+        self.send_cam_command('StartRecording')
         self.in_exec = True
         q_ext = suavizar(q)
-        self.mcu_server.send(q)
-        self.mcu_server.wait_exec() # Esperar mensaje de vuelta
-        self.send_command_str('StopRecording')
+        self.send_q_esp(q)
+        self.wait_exec_esp() # Esperar mensaje de vuelta
+        self.send_cam_command('StopRecording')
         q_out, p_out = alinear_datos(q_ext, self.p_stack)
         self.in_exec = False
         self.p_stack = []
 
         return q_out, p_out
+
+    def status(self):
+        mcu_status = self.serialESP is not None
+        cam_status = False # self.cam_client is not None
+        return (mcu_status, cam_status)
+
+
+if __name__ == "__main__":
+    client = ExtInstance()
