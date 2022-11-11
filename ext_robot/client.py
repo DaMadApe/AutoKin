ea0ddc3f70f587d6f06ecaafec01ebe2c5a6748e
damadape <danielsapien@gmail.com>
2022-11-11 07:06:10 -0600

    Ajustes generales del cliente externo

--- ext_robot/client.py
+++ ext_robot/client.py
@@ -1,3 +1,4 @@
+import torch
 import serial
 
 from ext_robot.NatNetClient import NatNetClient
@@ -16,57 +17,67 @@ class ExtInstance:
             self.serialESP = None
 
         self.cam_client = NatNetClient()
+        self.cam_client_connected = False
         self.cam_client.newFrameListener = self.recv_frame
         self.cam_client.rigidBodyListener = self.recv_body
-        self.cam_client.run() # En el mismo lugar que inicia SofaInstance
+        self.cam_client.run()
 
-        self.units = self.send_cam_command('UnitsToMillimeters')
-        self.frame_rate = self.send_cam_command('FrameRate')
+        # self.units = self.send_cam_command('UnitsToMillimeters')
+        # self.frame_rate = self.send_cam_command('FrameRate')
+
+        self.q_prev = [0]*4
 
         self.in_exec = False # Inecesario si se puede iniciar/detener stream
         self.p_stack = []
+
+    def test_connection(self, frameNumber, *args):
+        self.connected = True
 
     def recv_frame(self, frameNumber, 
                    markerSetCount,unlabeledMarkersCount,
                    rigidBodyCount,skeletonCount,labeledMarkerCount,
                    timecode,timecodeSub,timestamp,
                    isRecording,trackedModelsChanged):
+        if not self.cam_client_connected:
+            self.cam_client_connected = True
         print( "Frame", frameNumber )
+        # self.current_frame=frameNumber para tener esa info en recv_body?
 
     def recv_body(self, id, position, rotation):
         # called once per rigid body per frame
         print(f"Id: {id}, pos: {position}, rot: {rotation}")
-        if self.in_exec:
-            self.p_stack.append(position)
+        if self.in_exec: 
+            # if not self.current_frame % 100: # Limitar ritmo al que se guardan muestras
+            self.p_stack.append(list(position))
 
     def send_cam_command(self, command_str):
         self.cam_client.sendCommand(command=self.cam_client.NAT_REQUEST,
                                     commandStr=command_str,
-                                    # Modificar NatNetClient o crear fun
-                                    # diferente para facilitar esta llamada
                                     socket=self.cam_client.commandSocket,
                                     address=(self.cam_client.serverIPAddress,
-                                            self.cam_client.commandPort))
+                                             self.cam_client.commandPort))
 
     def send_q_esp(self, q): # -> ack # Saber cuando termina ejecución
         pasos = ['0']*4
         for i, val in enumerate(q):
-            pasos[i] = str(int(val))
+            pasos[i] = str(int(val - self.q_prev[i]))
+            self.q_prev[i] = val
         msg_pasos = ','.join(pasos)
+        # print(f'Estado de robot: {self.q_prev}, Mensaje: {msg_pasos}')
         if self.serialESP is not None:
             self.serialESP.write(msg_pasos.encode('ascii'))
-
-    def wait_exec_esp(self):
-        pass
+            # self.serialESP.read(4) # Para bloquear hasta que ESP envíe señal de fin
 
     def fkine(self, q):
-        self.send_cam_command('StartRecording')
+        # self.send_cam_command('StartRecording')
         self.in_exec = True
+
         q_ext = suavizar(q)
-        self.send_q_esp(q)
-        self.wait_exec_esp() # Esperar mensaje de vuelta
-        self.send_cam_command('StopRecording')
-        q_out, p_out = alinear_datos(q_ext, self.p_stack)
+        self.send_q_esp(q_ext)
+        # self.send_cam_command('StopRecording')
+        p = torch.tensor(self.p_stack)
+        q_out, p_out = alinear_datos(q_ext, p)
+
         self.in_exec = False
         self.p_stack = []
 
@@ -74,7 +85,7 @@ class ExtInstance:
 
     def status(self):
         mcu_status = self.serialESP is not None
-        cam_status = False # self.cam_client is not None
+        cam_status = self.cam_client_connected
         return (mcu_status, cam_status)
 
 
