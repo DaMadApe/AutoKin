db6ed20349d2102de2fc584bf64b21cbd3c3ce32
damadape <danielsapien@gmail.com>
2022-11-29 10:21:13 -0600

    Trayectoria de prueba para ensayar alineación

--- ext_robot/experimentos/prueba_alineacion.py
+++ ext_robot/experimentos/prueba_alineacion.py
@@ -3,13 +3,31 @@ import torch
 import numpy as np
 from matplotlib import pyplot as plt
 
-# ext_ds_dir = os.path.join('gui', 'app_data', 'robots', 'r3c', 'datasets')
-sofa_ds_dir = os.path.join('gui', 'app_data', 'robots', 'LSL', 'datasets')
+from autokin.robot import ExternRobot
+from autokin.muestreo import FKset
+from autokin.utils import restringir
 
-def load_last_ds(dir):
-    return torch.load(os.path.join(dir, sorted(os.listdir(dir))[-1]))
+DS_PATH = 'out_dataset.pt'
+N_PER_STEP = 100
 
-d_set = load_last_ds(sofa_ds_dir)
+robot = ExternRobot(n=3)
+
+test_trayec = torch.concat([torch.zeros(N_PER_STEP, robot.n),
+                            torch.tensor([1,0,0]).repeat(N_PER_STEP,1),
+                            torch.zeros(N_PER_STEP, robot.n),
+                            torch.tensor([0,1,0]).repeat(N_PER_STEP,1),
+                            torch.zeros(N_PER_STEP, robot.n),
+                            torch.tensor([0,0,1]).repeat(N_PER_STEP,1),
+                            torch.zeros(N_PER_STEP, robot.n),])
+
+test_trayec = restringir(test_trayec)
+
+d_set = FKset(robot, test_trayec)
+# dataset.robot = None
+torch.save(d_set, DS_PATH)
+
+# d_set = torch.load(os.path.join(dir, sorted(os.listdir(dir))[-1]))
+d_set = torch.load(DS_PATH)
 
 q_set = np.concatenate([d_point[0].unsqueeze(0).numpy() for d_point in d_set])
 p_set = np.concatenate([d_point[1].unsqueeze(0).numpy() for d_point in d_set])
