de150ab33b61f4daf0f689780024179ad5065872
damadape <danielsapien@gmail.com>
2022-11-29 10:32:26 -0600

    Mejorar visualización

--- ext_robot/experimentos/prueba_alineacion.py
+++ ext_robot/experimentos/prueba_alineacion.py
@@ -3,14 +3,14 @@ import torch
 import numpy as np
 from matplotlib import pyplot as plt
 
-from autokin.robot import ExternRobot
+from autokin.robot import ExternRobot, SofaRobot
 from autokin.muestreo import FKset
 from autokin.utils import restringir
 
 DS_PATH = 'out_dataset.pt'
 N_PER_STEP = 100
 
-robot = ExternRobot(n=3)
+robot = SofaRobot(config='LSL') #ExternRobot(n=3)
 
 test_trayec = torch.concat([torch.zeros(N_PER_STEP, robot.n),
                             torch.tensor([1,0,0]).repeat(N_PER_STEP,1),
@@ -23,11 +23,11 @@ test_trayec = torch.concat([torch.zeros(N_PER_STEP, robot.n),
 test_trayec = restringir(test_trayec)
 
 d_set = FKset(robot, test_trayec)
-# dataset.robot = None
-torch.save(d_set, DS_PATH)
+robot.stop_instance()
+# d_set.robot = None
+# torch.save(d_set, DS_PATH)
 
-# d_set = torch.load(os.path.join(dir, sorted(os.listdir(dir))[-1]))
-d_set = torch.load(DS_PATH)
+# d_set = torch.load(DS_PATH)
 
 q_set = np.concatenate([d_point[0].unsqueeze(0).numpy() for d_point in d_set])
 p_set = np.concatenate([d_point[1].unsqueeze(0).numpy() for d_point in d_set])
@@ -41,10 +41,10 @@ fig = plt.figure()
 ax = fig.add_subplot()
 
 # ax.scatter(t, q_diff, color='orangered')
-ax.plot(t, q_diff, color='orange')
+ax.plot(t, q_diff, color='orange', linewidth=1)
 
 # ax.scatter(t, p_diff, color='royalblue')
-ax.plot(t, 0.1*p_diff, color='lightblue')
+ax.plot(t, 0.1*p_diff, color='lightblue', linewidth=1)
 
 plt.tight_layout()
 plt.show()
