c761289d91a4c3d4b8ff8d8e85651f9266cb5e43
damadape <danielsapien@gmail.com>
2022-12-06 01:23:32 -0600

    Simplificar graficación de dataset

--- autokin/demos/demo_plot_dataset.py
+++ autokin/demos/demo_plot_dataset.py
@@ -13,15 +13,14 @@ ds_dir = os.path.join('gui', 'app_data', 'robots', robot_nom, 'datasets')
 
 d_set = torch.load(os.path.join(ds_dir, sorted(os.listdir(ds_dir))[-1]))
 
-p_set = np.concatenate([d_point[1].unsqueeze(0).numpy() for d_point in d_set])
+p_set = d_set[:][1].numpy().transpose()
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-p_trans = p_set.transpose()
 # p_trans = p_trans[:,::10] # Mostrar 1 de cada 10 puntos
-ax.scatter(*p_trans, color='royalblue')
-ax.plot(*p_trans, color='lightblue')
+ax.scatter(*p_set, color='royalblue')
+ax.plot(*p_set, color='lightblue')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
