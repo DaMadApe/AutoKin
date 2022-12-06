953198a6dff76d90a989881461c1b6a8fd32aa59
damadape <danielsapien@gmail.com>
2022-12-06 10:16:28 -0600

    Agregar suavizar a prueba de aceleración

--- ext_robot/experimentos/prueba_acc_trayec.py
+++ ext_robot/experimentos/prueba_acc_trayec.py
@@ -4,23 +4,28 @@ from torch.nn.functional import pad
 from matplotlib import pyplot as plt
 
 from autokin.trayectorias import coprime_sines
-from autokin.utils import restringir
+from autokin.utils import restringir, suavizar
 
-trayec = coprime_sines(3, 5000, 7, 0)
-q_plot = trayec.t().numpy()
+n = 3
+n_puntos = 5000
+
+trayec = coprime_sines(n, n_puntos, 7, 0)
+# trayec = suavizar(trayec, trayec[0], 0.1)
 
 qp = trayec.diff(dim=0).norm(dim=-1)
 qp = pad(qp, (0,1), mode='constant')
 qpp = trayec.diff(n=2, dim=0).norm(dim=-1)
 qpp = pad(qpp, (1,1), mode='constant')
 
-print(qp)
-
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
-ax.scatter(*q_plot, c=qp, cmap='turbo')
+q_plot = trayec.t().numpy()
+ax.scatter(*q_plot, c=qpp, cmap='turbo')
 # ax.scatter(*q_plot, c=qpp, cmap='turbo')
 
+print(f'Longitud de q: de {n_puntos} a {len(trayec)}')
+print(f'Max dq = {qp.max().item()}')
+print(f'Max d2q = {qpp.max().item()}')
 plt.tight_layout()
 plt.show()
