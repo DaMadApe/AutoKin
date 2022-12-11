1dfd3522fce8911e1e35274675b918891ae195d5
damadape <danielsapien@gmail.com>
2022-12-11 00:42:09 -0600

    Correcciones

--- ext_robot/experimentos/pruebas_fkine_ikine.py
+++ ext_robot/experimentos/pruebas_fkine_ikine.py
@@ -17,21 +17,22 @@ model_path = os.path.join('app_data', 'robots', nombre_robot,
                           'modelos', nombre_modelo+'.pt')
 
 model = ModelRobot.load(model_path,
-                        p_scale=torch.tensor([0.5, 0.5, 0.5]),
-                        p_offset=torch.tensor([ 1.0, 1.0, 1.0]))
+                        p_scale=torch.tensor([14.0707, 30.3070, 14.3431]),
+                        p_offset=torch.tensor([-0.5610, 5.6922, 0.3297]))
 
 d_set = torch.load(DS_PATH)
+d_set.apply_p_norm = False
 
 # Valores objetivo (tomar 1 de cada n)
-n = 10
+n = 100
 q_real = d_set[:][0][::n]
 p_real = d_set[:][1][::n]
+
+print(p_real)
 
 pred_q = []
 q_prev = torch.zeros(model.n)
 for i, p_si in enumerate(p_real):
-    print(q_prev)
-    p_target = p_si*model.p_scale + model.p_offset
     q = model.ikine_de(
         q_start=q_prev,
         p_target=p_si,
@@ -49,23 +50,23 @@ _, p_pred = model.fkine(q_real)
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1,2,1, projection='3d')
-ax1.set_title('Comparación modelo_CD(q_real) vs p_real')
-# p_trans = p_trans[:,::10] # Mostrar 1 de cada 10 puntos
+ax1.set_title('Modelo_CD(q_real) vs p_real')
 ax1.scatter(*p_pred.t().numpy(), color='royalblue')
 ax1.plot(*p_pred.t().numpy(), color='lightblue')
 ax1.scatter(*p_real.t().numpy(), color='red')
 ax1.plot(*p_real.t().numpy(), color='orange')
+ax1.legend(['modelo(q_real)', '', 'p_real', ''])
 ax1.set_xlabel('x')
 ax1.set_ylabel('y')
 ax1.set_zlabel('z')
 
 ax2 = fig.add_subplot(1,2,2, projection='3d')
 ax2.set_title('Parámetros encontrados por ikine vs q_real')
-# p_trans = p_trans[:,::10] # Mostrar 1 de cada 10 puntos
 ax2.scatter(*q_pred.t().numpy(), color='royalblue')
 ax2.plot(*q_pred.t().numpy(), color='lightblue')
 ax2.scatter(*q_real.t().numpy(), color='red')
 ax2.plot(*q_real.t().numpy(), color='orange')
+ax2.legend(['ikine(p_real)', '', 'q_real', ''])
 ax2.set_xlabel('q1')
 ax2.set_ylabel('q2')
 ax2.set_zlabel('q3')
